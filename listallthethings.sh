faf164e6f5c179476b6aff265c8898558c2eb0d4
Steven Pham <31803145+SPham17@users.noreply.github.com>
2022-03-31 16:56:58 -0400

    Add files via upload
    goes through all of the directories, running ls -l for the files that were given from arguments.

---
+++ listallthethings.sh
@@ -0,0 +1,21 @@
+
+#iterate through all of the directories
+for i in {0..100..1}
+do
+
+    #iterate through all of the argumeents
+    for j in "$@"
+    do
+	#check if file exists in directory
+	if [ -f "$j" ];
+	then
+	    ls -l $j
+#	    echo "File is in directory"
+#	else
+#	    echo "File is not in directory"
+	    #switch directory to previous directory
+	fi
+    done
+    #move to previous directory
+    cd ..
+done
