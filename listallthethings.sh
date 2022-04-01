cb77e87117318c2562460ec591c7506b98de0415
Steven Pham <31803145+SPham17@users.noreply.github.com>
2022-03-31 20:14:55 -0400

    Add files via upload
    Final, 
    outputs get put into the file
    Looks for elements with given arguments
    Also looks for the inode for given arguments

--- listallthethings.sh
+++ listallthethings.sh
@@ -1,3 +1,5 @@
+#always clear output.txt everytime it is run
+> output.txt
 
 #iterate through all of the directories
 for i in {0..100..1}
@@ -7,16 +9,41 @@ do
     for j in "$@"
     do
 	#check if file exists in directory
+	#does the longlist first
 	if [ -f "$j" ];
 	then
-	    ls -i $j
-#	    ls -l $j
+	    ls -l $j
+      	    echo "$(ls -l $j)" >> /home/pham8020/pham8020_a6/pham8020_q1/output.txt
+
 #	    echo "File is in directory"
 #	else
 #	    echo "File is not in directory"
 	    #switch directory to previous directory
 	fi
-    done
+
+    done 
+ 
+    for k in "$@"
+    do
+        #check if file exists in directory
+	#does the indoe list after the longlist
+        if [ -f "$k" ];
+        then
+            ls -i $k
+
+	    echo "$(ls -i $k)" >> /home/pham8020/pham8020_a6/pham8020_q1/output.txt
+ 
+
+
+#           echo "File is in directory"
+#       else
+#           echo "File is not in directory"
+            #switch directory to previous directory
+        fi
+    done 
+
+
+
     #move to previous directory
     cd ..
 done
